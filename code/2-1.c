28812041d6cb6861654c3528fd105f40a7abb8ba
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-03-13 09:55:11 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -2,6 +2,13 @@
 
 int main()
 {
+    printf("char: %lu\n", sizeof(char));
+    printf("short: %lu\n", sizeof(short));
+    printf("int: %lu\n", sizeof(int));
+    printf("unsigned int: %lu\n", sizeof(unsigned int));
+    printf("long: %lu\n", sizeof(long));
+    printf("long long: %lu\n", sizeof(long long));
+    printf("float: %lu\n", sizeof(float));
 
     return 0;
 }
