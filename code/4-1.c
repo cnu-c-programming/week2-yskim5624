2fd5f0a4e4cc00c6152cd8cd849cbe42859e45e0
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-03-13 17:26:42 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,20 @@
 
 int main()
 {
+    int a, b;
+    char c;
+
+    scanf("");
+    switch(c) {
+        case '+':
+        printf("%d", a+b);
+        case '-':
+        printf("%d", a-b);
+        case '*':
+        printf("%d", a*b);
+        case '/':
+        printf("%d", a/b);
+    }
 
     return 0;
 }
-
