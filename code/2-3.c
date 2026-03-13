dd784589c3625fd9eec1b92663e4ecb24ebe7355
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-03-13 10:02:37 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -2,7 +2,14 @@
 
 int main()
 {
+    unsigned int x = 0x12345678;
+    
+    unsigned char* addr = (unsigned char*)&x;
+
+    printf("%x\n", *addr);
+    printf("%x\n", *(addr + 1));
+    printf("%x\n", *(addr + 2));
+    printf("%x\n", *(addr + 3));
 
     return 0;
 }
-
