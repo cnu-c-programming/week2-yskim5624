44b1cdc89f193b5f902d1a999a46f7a924159deb
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-03-19 23:40:16 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -5,11 +5,11 @@ int main()
     int n, count=0;
 
     scanf("%d", &n);
+    if(n==0 || n==1) count++;
     for(int i=2; i<n; i++) {
         if(n%i==0) count++;
     }
     if(count==0) printf("true");
-    else if(n==0 || n==1) printf("false");
     else printf("false");
     
     return 0;
