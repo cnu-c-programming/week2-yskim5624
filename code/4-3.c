d43003aa5584bc6bebbccc50914320e7f8e08471
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-03-19 19:30:08 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,14 @@
 
 int main()
 {
+    int n, count=0;
 
+    scanf("%d", &n);
+    for(int i=2; i<n; i++) {
+        if(n%i==0) count++;
+    }
+    if(count==0) printf("true");
+    else printf("false");
+    
     return 0;
 }
-
