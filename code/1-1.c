1196d9d818b0202263d3329d9e91f805a633dab8
yskim5624 <139705489+yskim5624@users.noreply.github.com>
2026-03-19 23:30:19 +0900

    Change printf format specifiers for integers

--- code/1-1.c
+++ code/1-1.c
@@ -4,8 +4,8 @@ int main()
 {
     int var1 = 100;
     printf("int : %d\n", var1);
-    printf("int : %x\n", var1);
-    printf("int : %o\n", var1);
+    printf("hex : %x\n", var1);
+    printf("oct : %o\n", var1);
 
     char var2 = 'c';
     printf("char : %c\n", var2);
@@ -16,7 +16,6 @@ int main()
     float var4 = 3.141592;
     printf("float : %f\n", var4);
     printf("exp : %e\n", var4);
-
     
     return 0;
 }
